64833b18730005936cce2e0baeacac5068abbd9e
Zach Tibbitts <zachtib@gmail.com>
2011-07-06 21:13:55 -0400

    Add the template (not quite done)

--- resume/views.py
+++ resume/views.py
@@ -2,31 +2,19 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.admin.models import User
 from django.template import RequestContext
 
-from resume.models import Resume, Degree, Job, Project, Skill, Extracurricular
+from resume.models import Degree, Job, Project, Skill, Extracurricular
 
 def index(request, noheader=False):
-    r = get_object_or_404(Resume, pk=1)
-    d = Degree.objects.filter(resume=r).order_by('-graddate')
-    j = Job.objects.filter(resume=r).order_by('-startdate')
-    p = Project.objects.filter(resume=r).order_by('-begindate')
-    s = Skill.objects.filter(resume=r)
-    e = Extracurricular.objects.filter(resume=r)
-    if noheader:
-        return render_to_response('resume/noheader.html',
-                                {   'resume': r,
-                                    'degrees': d,
+    d = Degree.objects.all()
+    j = Job.objects.all()
+    p = Project.objects.all()
+    s = Skill.objects.all()
+    e = Extracurricular.objects.all()
+
+    return render_to_response('index.html',
+                                {   'degrees': d,
                                     'jobs': j,
                                     'projects': p,
                                     'skills': s,
                                     'extras': e,
-                                },
-                                context_instance=RequestContext(request))
-    return render_to_response('resume/index.html',
-                                {   'resume': r,
-                                    'degrees': d,
-                                    'jobs': j,
-                                    'projects': p,
-                                    'skills': s,
-                                    'extras': e,
-                                },
-                                context_instance=RequestContext(request))
+                                })
