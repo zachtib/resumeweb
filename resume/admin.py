1a3d76fcca24cae1042dee270e33083e9f6bb3a9
Zach Tibbitts <zachtib@gmail.com>
2011-07-06 21:03:02 -0400

    get rid of the overall Resume model, since this is going to be one item per installation

--- resume/admin.py
+++ resume/admin.py
@@ -1,33 +1,11 @@
-from resume.models import Resume, Degree, Job, Project, Skill, Extracurricular
+from resume.models import Degree, Job, Project, Skill, Extracurricular
 from django.contrib import admin
 
-class DegreeInline(admin.StackedInline):
-    model = Degree
-    extra = 1
+admin.site.register(Degree)
+admin.site.register(Job)
+admin.site.register(Project)
+admin.site.register(Skill)
+admin.site.register(Extracurricular)
 
-class JobInline(admin.StackedInline):
-    model = Job
-    extra = 1
 
-class ProjectInline(admin.StackedInline):
-    model = Project
-    extra = 1
-
-class SkillInline(admin.StackedInline):
-    model = Skill
-    extra = 1
-
-class ExtracurricularInline(admin.TabularInline):
-    fields = ['description', 'begindate', 'enddate']
-    model = Extracurricular
-    extra = 1
-
-class ResumeAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ('General', {'fields': ['user', 'name', 'objective']})
-    ]
-    inlines = [DegreeInline, JobInline, ProjectInline, SkillInline,
-        ExtracurricularInline]
-
-admin.site.register(Resume, ResumeAdmin)
 
--- resume/models.py
+++ resume/models.py
@@ -5,22 +5,12 @@ class DateRange(models.Model):
     start = models.DateField('Start Date')
     end = models.DateField('End Date')
 
-class Resume(models.Model):
-    user = models.ForeignKey(User)
-    name = models.CharField('Full Name', max_length=200)
-    objective = models.TextField()
-
-    def __unicode__(self):
-        return 'Resume for %s' % self.name
-
 class Degree(models.Model):
-    resume = models.ForeignKey(Resume)
     school = models.CharField(max_length=200)
     degree = models.CharField(max_length=200)
     graddate = models.DateField('Graduation Date')
 
 class Job(models.Model):
-    resume = models.ForeignKey(Resume)
     company = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
     position = models.CharField(max_length=200)
@@ -29,19 +19,16 @@ class Job(models.Model):
     description = models.TextField()
 
 class Project(models.Model):
-    resume = models.ForeignKey(Resume)
     name = models.CharField(max_length=200)
     begindate = models.DateField('Begin Date')
     enddate = models.DateField('End Date', null=True, blank=True)
     description = models.TextField()
 
 class Skill(models.Model):
-    resume = models.ForeignKey(Resume)
     title = models.CharField(max_length=200)
     description = models.TextField()
 
 class Extracurricular(models.Model):
-    resume = models.ForeignKey(Resume)
     begindate = models.DateField('Begin Date')
     enddate = models.DateField('End Date', null=True, blank=True)
     description = models.CharField(max_length=200)
