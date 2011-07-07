0e726ef9804710153aba23e090a6c7d9f42577d8
Zach Tibbitts <zachtib@gmail.com>
2011-07-06 21:32:24 -0400

    Further remove the old Resume model

--- resume/admin.py
+++ resume/admin.py
@@ -1,6 +1,7 @@
-from resume.models import Degree, Job, Project, Skill, Extracurricular
+from resume.models import ContactInformation, Degree, Job, Project, Skill, Extracurricular
 from django.contrib import admin
 
+admin.site.register(ContactInformation)
 admin.site.register(Degree)
 admin.site.register(Job)
 admin.site.register(Project)
--- resume/models.py
+++ resume/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
 from django.db import models
+
+class ContactInformation(models.Model):
+    site = models.OneToOneField(Site)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    phone = models.CharField(max_length=200, null=True, blank=True)
+    objective = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
 
 class DateRange(models.Model):
     start = models.DateField('Start Date')
